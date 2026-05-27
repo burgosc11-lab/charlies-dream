c37f33db888f2dc3b9bd1f9d2146584e6ea5dcc0
burgosc11-lab <burgosc11-lab@users.noreply.github.com>
2026-05-27 13:03:55 -0600

    Remove in-app notification strip

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE = 'the-list-v15';
+const CACHE = 'the-list-v16';
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(['index.html', 'icon-192.png'])));
