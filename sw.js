ecd8e8c09d51507b456365b817fae71760bdb85a
burgosc11-lab <burgosc11@gmail.com>
2026-06-01 13:16:51 -0600

    chore: bump cache to v17 to deploy sync feature

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE = 'the-list-v16';
+const CACHE = 'the-list-v17';
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(['index.html', 'icon-192.png'])));
