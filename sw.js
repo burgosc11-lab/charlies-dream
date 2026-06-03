257dd18cb5525fa019bb57314445b10359c0717b
burgosc11-lab <burgosc11@gmail.com>
2026-06-03 13:48:21 -0600

    chore: bump cache to the-list-v28

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE = 'the-list-v27';
+const CACHE = 'the-list-v28';
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(['index.html', 'icon-192.png'])));
